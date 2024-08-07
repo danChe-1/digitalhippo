ae1c53363140a0e1a918e031641dc9fc97f533a9
Daniel <dan.cherkasky@gmail.com>
2024-08-07 10:22:29 +0300

    finalizing

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,13 +1,47 @@
-import { CollectionConfig } from "payload/types";
+import { Access, CollectionConfig } from "payload/types";
+const adminsAndUser: Access = ({ req: { user } }) => {
+  if (user.role === "admin") return true;
 
+  return {
+    id: {
+      equals: user.id,
+    },
+  };
+};
 export const Users: CollectionConfig = {
   slug: "users",
   auth: true,
   access: {
-    read: () => true,
+    read: adminsAndUser,
     create: () => true,
+    update: ({ req }) => req.user.role === "admin",
+    delete: ({ req }) => req.user.role === "admin",
+  },
+  admin: {
+    hidden: ({ user }) => user.role !== "admin",
+    defaultColumns: ["id"],
   },
   fields: [
+    {
+      name: "products",
+      label: "Products",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "products",
+      hasMany: true,
+    },
+    {
+      name: "product_files",
+      label: "Product Files",
+      admin: {
+        condition: () => false,
+      },
+      type: "relationship",
+      relationTo: "product_files",
+      hasMany: true,
+    },
     {
       name: "role",
       defaultValue: "user",
--- src/payload-types.ts
+++ src/payload-types.ts
@@ -13,8 +13,8 @@ export interface Config {
     media: Media;
     product_files: ProductFile;
     orders: Order;
-    "payload-preferences": PayloadPreference;
-    "payload-migrations": PayloadMigration;
+    'payload-preferences': PayloadPreference;
+    'payload-migrations': PayloadMigration;
   };
   globals: {};
 }
@@ -24,7 +24,9 @@ export interface Config {
  */
 export interface User {
   id: string;
-  role?: ("admin" | "user") | null;
+  products?: (string | Product)[] | null;
+  product_files?: (string | ProductFile)[] | null;
+  role?: ('admin' | 'user') | null;
   updatedAt: string;
   createdAt: string;
   email: string;
@@ -46,9 +48,9 @@ export interface Product {
   name: string;
   description?: string | null;
   price: number;
-  category: "ui_kits" | "icons";
+  category: 'ui_kits' | 'icons';
   product_files: string | ProductFile;
-  approvedForSale?: ("pending" | "approved" | "denied") | null;
+  approvedForSale?: ('pending' | 'approved' | 'denied') | null;
   priceId?: string | null;
   stripeId?: string | null;
   images: {
@@ -139,7 +141,7 @@ export interface Order {
 export interface PayloadPreference {
   id: string;
   user: {
-    relationTo: "users";
+    relationTo: 'users';
     value: string | User;
   };
   key?: string | null;
@@ -165,4 +167,9 @@ export interface PayloadMigration {
   batch?: number | null;
   updatedAt: string;
   createdAt: string;
+}
+
+
+declare module 'payload' {
+  export interface GeneratedTypes extends Config {}
 }
